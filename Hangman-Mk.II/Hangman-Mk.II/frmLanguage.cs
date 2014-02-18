a22388b4e70f3162dc0824134d363d789204c80f
joshwalls <josh@wallsfamily.co.uk>
2014-02-18 15:13:32 +0000

    0.0.2

---
+++ Hangman-Mk.II/Hangman-Mk.II/frmLanguage.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Hangman_Mk.II
+{
+	public partial class frmLanguage : Form
+	{
+		public frmLanguage()
+		{
+			InitializeComponent();
+		}
+
+		string[] words = new string[] { };
+		string word;
+		int wordsLength;
+		int randomNumber;
+		char[] wordArray = new char[] { };
+		DialogResult result;
+		string wordStars;
+
+	
+
+		
+
+		private void btnLanguageGo_Click(object sender, EventArgs e)
+		{
+			if (comboBoxLanguage.Text == "Debug")
+			{
+				words = File.ReadAllLines("Resources/debug.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Afrikaans")
+			{
+				words = File.ReadAllLines("Resources/afrikaans.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Belarusian")
+			{
+				words = File.ReadAllLines("Resources/belarusian.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Bulgarian")
+			{
+				words = File.ReadAllLines("Resources/bulgarian.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Catalan")
+			{
+				words = File.ReadAllLines("Resources/catalan.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Croatian")
+			{
+				words = File.ReadAllLines("Resources/croatian.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Czech")
+			{
+				words = File.ReadAllLines("Resources/czech.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Danish")
+			{
+				words = File.ReadAllLines("Resources/danish.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Dutch")
+			{
+				words = File.ReadAllLines("Resources/dutch.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "English")
+			{
+				words = File.ReadAllLines("Resources/english.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "English (US)")
+			{
+				words = File.ReadAllLines("Resources/american.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "French")
+			{
+				words = File.ReadAllLines("Resources/french.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "German")
+			{
+				words = File.ReadAllLines("Resources/german.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Greek")
+			{
+				words = File.ReadAllLines("Resources/greek.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Hungarian")
+			{
+				words = File.ReadAllLines("Resources/hungarian.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Italian")
+			{
+				words = File.ReadAllLines("Resources/italian.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Latin")
+			{
+				words = File.ReadAllLines("Resources/latin.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Mongolian")
+			{
+				words = File.ReadAllLines("Resources/mongolian.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Polish")
+			{
+				words = File.ReadAllLines("Resources/polish.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Portuguese")
+			{
+				words = File.ReadAllLines("Resources/portuguese.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Romanian")
+			{
+				words = File.ReadAllLines("Resources/romanian.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Russian")
+			{
+				words = File.ReadAllLines("Resources/russian.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Serbian")
+			{
+				words = File.ReadAllLines("Resources/serbian.txt");
+				wordsLength = words.Length;
+
+			}
+			else if (comboBoxLanguage.Text == "Slovak")
+			{
+				words = File.ReadAllLines("Resources/slovak.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Slovenian")
+			{
+				words = File.ReadAllLines("Resources/slovianian.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Spanish")
+			{
+				words = File.ReadAllLines("Resources/spanish.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Swedish")
+			{
+				words = File.ReadAllLines("Resources/swedish.txt");
+				wordsLength = words.Length;
+			}
+			else if (comboBoxLanguage.Text == "Ukrainian")
+			{
+				words = File.ReadAllLines("Resources/ukranian.txt");
+				wordsLength = words.Length;
+			}
+			else
+			{
+				MessageBox.Show("Sorry, we don't have that language" + "\n" + "Select one from the list");
+			}
+
+
+
+			for (int i = 0; i < words.Length; i++)
+			{
+				words[i] = words[i].ToUpper();
+			}
+
+			Random random = new Random();
+			randomNumber = random.Next(0, wordsLength);
+			word = words[randomNumber];
+
+			if (result == DialogResult.Yes)
+			{
+				MessageBox.Show("dictionary lenght: " + wordsLength + "\nrandom number selected: " + randomNumber + "\nword: " + word + "\n");
+			}
+
+			wordArray = word.ToCharArray();
+
+			foreach (char c in word)
+			{
+				wordStars += "*";
+			}
+		}
+
+		private void radioButtonLanguage_CheckedChanged(object sender, EventArgs e)
+		{
+			
+		}
+
+
+		
+	}
+}
---
+++ Hangman-Mk.II/Hangman-Mk.II/frmStart.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Hangman_Mk.II
+{
+	partial class frmStart
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.labelSelectedLanguage = new System.Windows.Forms.Label();
+			this.buttonPlay = new System.Windows.Forms.Button();
+			this.buttonChoose = new System.Windows.Forms.Button();
+			this.buttonQuit = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// labelSelectedLanguage
+			// 
+			this.labelSelectedLanguage.AutoSize = true;
+			this.labelSelectedLanguage.Location = new System.Drawing.Point(520, 140);
+			this.labelSelectedLanguage.Name = "labelSelectedLanguage";
+			this.labelSelectedLanguage.Size = new System.Drawing.Size(35, 13);
+			this.labelSelectedLanguage.TabIndex = 0;
+			this.labelSelectedLanguage.Text = "label1";
+			// 
+			// buttonPlay
+			// 
+			this.buttonPlay.Location = new System.Drawing.Point(193, 127);
+			this.buttonPlay.Name = "buttonPlay";
+			this.buttonPlay.Size = new System.Drawing.Size(75, 23);
+			this.buttonPlay.TabIndex = 3;
+			this.buttonPlay.Text = "Play!";
+			this.buttonPlay.UseVisualStyleBackColor = true;
+			this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+			// 
+			// buttonChoose
+			// 
+			this.buttonChoose.Location = new System.Drawing.Point(193, 168);
+			this.buttonChoose.Name = "buttonChoose";
+			this.buttonChoose.Size = new System.Drawing.Size(75, 23);
+			this.buttonChoose.TabIndex = 4;
+			this.buttonChoose.Text = "Choose Language";
+			this.buttonChoose.UseVisualStyleBackColor = true;
+			this.buttonChoose.Click += new System.EventHandler(this.buttonChoose_Click);
+			// 
+			// buttonQuit
+			// 
+			this.buttonQuit.Location = new System.Drawing.Point(193, 198);
+			this.buttonQuit.Name = "buttonQuit";
+			this.buttonQuit.Size = new System.Drawing.Size(75, 23);
+			this.buttonQuit.TabIndex = 5;
+			this.buttonQuit.Text = "Quit";
+			this.buttonQuit.UseVisualStyleBackColor = true;
+			this.buttonQuit.Click += new System.EventHandler(this.buttonQuit_Click);
+			// 
+			// frmStart
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(684, 361);
+			this.Controls.Add(this.buttonQuit);
+			this.Controls.Add(this.buttonChoose);
+			this.Controls.Add(this.buttonPlay);
+			this.Controls.Add(this.labelSelectedLanguage);
+			this.Name = "frmStart";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label labelSelectedLanguage;
+		private System.Windows.Forms.Button buttonPlay;
+		private System.Windows.Forms.Button buttonChoose;
+		private System.Windows.Forms.Button buttonQuit;
+	}
+}
+
